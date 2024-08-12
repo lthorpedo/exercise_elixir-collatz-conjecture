17b15802d8e5b9f984a2e6896533de72e2776270
Levi Thorpe <56571505+lthorpedo@users.noreply.github.com>
2024-08-12 14:28:46 -0600

    rework the guards to prevent the function from being called, so that the BEAM VM raises the FunctionClauseError instead of me doing it manually

--- lib/collatz_conjecture.ex
+++ lib/collatz_conjecture.ex
@@ -1,5 +1,6 @@
 defmodule CollatzConjecture do
-  defguardp is_even(term) when is_integer(term) and rem(term, 2) == 0
+  defguardp is_even(term) when is_integer(term) and term > 0 and rem(term, 2) == 0
+  defguardp is_positive(t) when is_integer(t) and t > 0
   @doc """
   calc/1 takes an integer and returns the number of steps required to get the
   number to 1 when following the rules:
@@ -7,12 +8,10 @@ defmodule CollatzConjecture do
     - if number is even, divide by 2
   """
   @spec calc(input :: pos_integer()) :: non_neg_integer()
-  def calc(input) when not is_integer(input), do: raise FunctionClauseError
-  def calc(input) when input < 1, do: raise FunctionClauseError
   def calc(input) when input == 1, do: 0
-  def calc(input) when is_even(input) do
+  def calc(input) when is_positive(input) and is_even(input) do
     x = (input / 2) |> round |> calc
     x + 1
   end
-  def calc(input), do: 1 + calc((3 * input + 1))
+  def calc(input) when is_positive(input), do: 1 + calc((3 * input + 1))
 end
